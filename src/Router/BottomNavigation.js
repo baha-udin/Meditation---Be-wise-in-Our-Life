2e411f11da201151fb95a03a0be61047b21abb22
Muhamad Baha Udin <53718689+baha-udin@users.noreply.github.com>
2022-02-04 17:38:42 +0700

    add bottom navigation

--- src/Router/BottomNavigation.js
+++ src/Router/BottomNavigation.js
@@ -1,34 +1,200 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import {Home, Sleep, Meditate, Music, Profile} from './../pages';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {
+  heroImage,
+  IconHome,
+  IconHomeActive,
+  IconMeditate,
+  IconMeditateActive,
+  IconMusic,
+  IconMusicActive,
+  IconSleep,
+  IconSleepActive,
+  IconProfile,
+  IconProfileActive,
+} from './..//assets/img';
+import Colors from './../assets/Colors';
 
 const Tab = createBottomTabNavigator();
 
-const BottomNavigation = () => {
+const BottomNavigation = ({navigation}) => {
   // Tab.Navigator ini nampung bottom menu
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" options={{headerShown: false}} component={Home} />
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: Colors.Basic_BG,
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarStyle: {
+          position: 'absolute',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: 60,
+          width: '100%',
+          bottom: 0,
+          backgroundColor: Colors.Basic_BG,
+          borderRadius: 10,
+          paddingVertical: 6,
+          ...styles.tabBarStyling,
+        },
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                paddingVertical: 8,
+                paddingHorizontal: 8,
+                borderRadius: 12,
+                backgroundColor: focused ? Colors.BUTTON_BG : Colors.Basic_BG,
+              }}>
+              <Image
+                source={focused ? IconHomeActive : IconHome}
+                style={{width: 24, height: 24, opacity: 0.8}}
+              />
+              <Text
+                style={{
+                  fontSize: 12,
+                  marginTop: 4,
+                  color: focused ? Colors.FontColor : '',
+                  opacity: focused ? 1 : 0.6,
+                }}>
+                Home
+              </Text>
+            </View>
+          ),
+        }}
+      />
       <Tab.Screen
         name="Sleep"
-        options={{headerShown: false}}
         component={Sleep}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                paddingVertical: 8,
+                paddingHorizontal: 8,
+                borderRadius: 12,
+                backgroundColor: focused ? Colors.BUTTON_BG : Colors.Basic_BG,
+              }}>
+              <Image
+                source={focused ? IconSleepActive : IconSleep}
+                style={{width: 24, height: 24, opacity: 0.8}}
+              />
+              <Text
+                style={{
+                  fontSize: 12,
+                  marginTop: 4,
+                  color: focused ? Colors.FontColor : '',
+                  opacity: focused ? 1 : 0.6,
+                }}>
+                Sleep
+              </Text>
+            </View>
+          ),
+        }}
       />
       <Tab.Screen
         name="Meditate"
-        options={{headerShown: false}}
         component={Meditate}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                paddingVertical: 8,
+                paddingHorizontal: 8,
+                borderRadius: 12,
+                backgroundColor: focused ? Colors.BUTTON_BG : Colors.Basic_BG,
+              }}>
+              <Image
+                source={focused ? IconMeditateActive : IconMeditate}
+                style={{width: 24, height: 24, opacity: 0.8}}
+              />
+              <Text
+                style={{
+                  fontSize: 12,
+                  marginTop: 4,
+                  color: focused ? Colors.FontColor : '',
+                  opacity: focused ? 1 : 0.6,
+                }}>
+                Meditate
+              </Text>
+            </View>
+          ),
+        }}
       />
       <Tab.Screen
         name="Music"
-        options={{headerShown: false}}
         component={Music}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                paddingVertical: 8,
+                paddingHorizontal: 8,
+                borderRadius: 12,
+                backgroundColor: focused ? Colors.BUTTON_BG : Colors.Basic_BG,
+              }}>
+              <Image
+                source={focused ? IconMusicActive : IconMusic}
+                style={{width: 24, height: 24, opacity: 0.8}}
+              />
+              <Text
+                style={{
+                  fontSize: 12,
+                  marginTop: 4,
+                  color: focused ? Colors.FontColor : '',
+                  opacity: focused ? 1 : 0.6,
+                }}>
+                Music
+              </Text>
+            </View>
+          ),
+        }}
       />
       <Tab.Screen
         name="Profile"
-        options={{headerShown: false}}
         component={Profile}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                paddingVertical: 8,
+                paddingHorizontal: 8,
+                borderRadius: 12,
+                backgroundColor: focused ? Colors.BUTTON_BG : Colors.Basic_BG,
+              }}>
+              <Image
+                source={focused ? IconProfileActive : IconProfile}
+                style={{width: 24, height: 24, opacity: 0.8}}
+              />
+              <Text
+                style={{
+                  fontSize: 12,
+                  marginTop: 4,
+                  color: focused ? Colors.FontColor : '',
+                  opacity: focused ? 1 : 0.6,
+                }}>
+                Profile
+              </Text>
+            </View>
+          ),
+        }}
       />
     </Tab.Navigator>
   );
@@ -36,4 +202,15 @@ const BottomNavigation = () => {
 
 export default BottomNavigation;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  tabBarStyling: {
+    shadowColor: '#7f5df0',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 10,
+  },
+});
